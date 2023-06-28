0d081bfe14571f59fbbfebf9c17b1fdd15857f6b
João Francisco <joao.rodrigues@viasoft.com.br>
2023-06-28 20:33:12 -0300

    refactor: prettier

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,18 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { Post, User } from './entity';
+import { Init1687995063628 } from './migration/1687995063628-init';
 
 export const AppDataSource = new DataSource({
-  type: "mysql",
-  host: "localhost",
+  type: 'mysql',
+  host: 'localhost',
   port: 3306,
-  username: "admin",
-  password: "123456",
-  database: "reddit-clone",
+  username: 'admin',
+  password: '123456',
+  database: 'reddit-clone',
   synchronize: true,
   logging: false,
-  entities: ["../entity/index{.js,.ts}"],
-  migrations: ["../migration/index{.js.ts}"],
-  migrationsTableName: "migrations",
+  entities: [User, Post],
+  migrations: [Init1687995063628],
+  migrationsTableName: 'migrations',
 });
